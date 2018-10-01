cde0dcad64016e230da51d863b3e854d233dfc4a
Andrey Zakharov <aazaharov81@gmail.com>
2018-10-01 19:51:33 +0300

    Made most final changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input, 
-	OnInit, OnChanges, SimpleChanges, SimpleChange } from '@angular/core';
+import { Component, Input,
+    OnInit, OnChanges, SimpleChanges, SimpleChange } from '@angular/core';
 import { RecipesService } from './recipes.service';
 import { Observable } from 'rxjs';
 import { format as d3format, scaleOrdinal, schemeCategory10 } from 'd3';
@@ -13,161 +13,156 @@ import {MatMenuModule} from '@angular/material/menu';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnChanges, OnInit {
-	title = 'factorio-recipes';
-	//recipes: Map<string, any> = new Map();
-	recipes : Object;
-	categories: Map<string, Array> = new Map();
-	svg;
-	@Input() selectedRecipeId : string;
+    title = 'factorio-recipes';
+    recipes: Object;
+    categories: Map<string, Array> = new Map();
+    svg;
+    @Input() selectedRecipeId: string;
 
-	constructor( private data: RecipesService ) {} 
+    constructor( private data: RecipesService ) {}
 
-	ngOnInit(): void {
-		this.data.getRecipes().subscribe(data => this.ongetrecipes(data));
-		this.svg = d3.select("svg");
+    ngOnInit(): void {
+        this.data.getRecipes().subscribe(data => this.ongetrecipes(data));
+        this.svg = d3.select('svg');
 
-	}
+    }
 
- 	ngOnChanges(changes: SimpleChanges) {
- 		console.log(changes);
-    	//const name: SimpleChange = changes.selectedRecipeId;
+    ngOnChanges(changes: SimpleChanges) {
+        console.log(changes);
+        // const name: SimpleChange = changes.selectedRecipeId;
     }
 
     onrecipeselect(rid) {
-    	this.selectedRecipeId = rid;
-    	this.drawGraphFor(rid);
+        this.selectedRecipeId = rid;
+        this.drawGraphFor(rid);
     }
 
 
-	ongetrecipes(data) {
-		this.recipes = data;
+    ongetrecipes(data) {
+        this.recipes = data;
 
-		for( let [k, r] of Object.entries(this.recipes) ) {
-			if ( r.recipe.ingredients.length == 0 ) continue;
+        for ( const [k, r] of Object.entries( this.recipes ) ) {
+            if ( r.recipe.ingredients.length === 0 ) { continue; }
 
-			if ( ! this.categories.has( r.type ) ) {
-				this.categories.set( r.type, [] );
-			}
+            if ( ! this.categories.has( r.type ) ) {
+                this.categories.set( r.type, [] );
+            }
 
-			this.categories.get( r.type ).push([r.id, r.name]);
-		}
-		
-		this.drawGraphFor("atomic-bomb");
-	}
+            this.categories.get( r.type ).push([r.id, r.name]);
+        }
 
-	onchange(e) {
-		this.drawGraphFor( e.target.value );
-	}
+        this.drawGraphFor('atomic-bomb');
+    }
 
-	drawGraphFor( recipeId ) {
-	
-		console.group(recipeId);
+    onchange(e) {
+        this.drawGraphFor( e.target.value );
+    }
 
-		const width : int = 800;
-		const height : int = 600;
+    drawGraphFor( recipeId ) {
 
-		let [nodesMap, links] = this.getSourcesFor( recipeId );
-		let nodes : Array = Array.from(nodesMap.values());
-		console.debug("raw nodes", nodes);
-		console.debug("raw links", links);
+        // console.group(recipeId);
 
-	 	const scale = scaleOrdinal(schemeCategory10)
-	    const color = name => scale(name.replace(/ .*/, ''));
-	    const format = d3format(".2g");
-		{links, nodes} = d3Sankey()
-			.nodeId(d=>d.id)
-			// .iterations(100)
-			.extent([[1, 1], [width - 1, height - 5]])
-		({ 
-			//nodes: [ {id: "a", name: "test1"}, {id: "b", name: "another-test"}, { id:"c", name: "third test"}],
-			nodes: nodes, links: links,
-			// links: [ 
-			// 	{source: "a", target: "b", type: "x", value: 2},
-			// 	{source: "a", target: "c", type: "y", value: 3},
-			// ]
-		});
+        const width: int = 800;
+        const height: int = 600;
 
-		console.debug("result nodes", nodes);
-		console.debug("result links", links);
-		console.groupEnd();
-		this.svg.selectAll("*").remove();
-		this.svg.attr("viewBox", `0 0 ${width} ${height}`);
-	  	const link = this.svg.append("g")
-    		.attr("fill", "none")
-    		.attr("stroke", "#000")
-    		.attr("stroke-opacity", 0.2)
-  		.selectAll("path")
-  		.data(links)
-  		.enter().append("path")
-  			.attr("stroke", d => color(d.source.name))
-    		.attr("d", sankeyLinkHorizontal())
-    		.attr("stroke-width", function(d) { return d.width; });
+        const [nodesMap, links] = this.getSourcesFor( recipeId );
+        const nodes: Array = Array.from(nodesMap.values());
+        // console.debug('raw nodes', nodes);
+        // console.debug('raw links', links);
 
-		link.append("title")
-			.text(d => `${d.source.name} → ${d.target.name}\n${format(d.value)}`);
+        const scale = scaleOrdinal(schemeCategory10);
+        const color = name => scale(name.replace(/ .*/, ''));
+        const format = d3format('.2g');
+        const sankeyDiagram = d3Sankey()
+            .nodeId( d => d.id )
+            // .iterations(100)
+            .extent( [[1, 1], [width - 1, height - 5]] )
+            .nodes(nodes)
+            .links(links);
 
-		this.svg.append("g")
-      		.attr("stroke", "#000")
-    	.selectAll("rect")
-	    .data(nodes)
-	    .enter().append("rect")
-      		.attr("x", d => d.x0)
-			.attr("y", d => d.y0)
-			.attr("height", d => d.y1 - d.y0)
-			.attr("width", d => d.x1 - d.x0)
-			.attr("fill", d => color(d.name))
-	    .append("title")
-      		.text(d => `${d.name}\ninput ${format(d.value)} items per second`);
+        const {links, nodes} = sankeyDiagram();
 
-  		this.svg.append("g")
-	      	.selectAll('rect')
-	    	.data(nodes)
-	        .enter()
-	        	.append("text")
-	        		.attr('fill', d => color(d.name))
-			      	.attr("x", d => d.x0 < width / 2 ? d.x1 + 6 : d.x0 - 6)
-			      	.attr("y", d => (d.y1 + d.y0) / 2)
-		      		.attr("text-anchor", d => d.x0 < width / 2 ? "start" : "end")
-		      		.attr("dy", "0.35em")
-		      		.text(d => d.name);
+        // console.debug('result nodes', nodes);
+        // console.debug('result links', links);
+        // console.groupEnd();
+        this.svg.selectAll('*').remove();
+        this.svg.attr('viewBox', `0 0 ${width} ${height}`);
+        const link = this.svg.append('g')
+            .attr('fill', 'none')
+            .attr('stroke', '#000')
+            .attr('stroke-opacity', 0.2)
+        .selectAll('path')
+        .data(links)
+        .enter().append('path')
+            .attr('stroke', d => color(d.source.name))
+            .attr('d', sankeyLinkHorizontal())
+            .attr('stroke-width', function(d) { return d.width; });
 
-	}
+        link.append('title')
+            .text(d => `${d.source.name} → ${d.target.name}\n${format(d.value)}`);
 
-	// return nodes, links
-	getSourcesFor( recipeId ) {
-		let nodes = new Map<string, any>();
-		let links = [];
-		//console.group(recipeId);
-		nodes.set(recipeId, {id: recipeId, name: this.recipes[recipeId].name});
-		
-		for ( const ingr of this.recipes[recipeId].recipe.ingredients ) {
-			
-			//nodes.set(ingr.id, {id: ingr.id, name: this.recipes[ingr.id].name});
-			links.push( { 
-				source: ingr.id , target: recipeId, 
-				value: ingr.amount / this.recipes[recipeId].recipe.time / this.recipes[recipeId].recipe.yield 
-			} );
-			const [childNodes, childLinks] = this.getSourcesFor(ingr.id);
+        const node = this.svg.append('g')
 
-			childNodes.forEach(function (v, k, m) {
-				nodes.set(k, v);
-			});
+        .selectAll('rect')
+        .data(nodes)
+        .enter()
+            .append('g');
 
-			for( let chL of childLinks ) { //sum
-				let found = links.find( el => el.source == chL.source && el.target == chL.target );
-				if ( !!found ) {
-					found.value += chL.value;
-				} else {
-					links.push( chL );
-				}
-			}
+        node.append('rect')
+                .attr('stroke', '#222')
+                .attr('x', d => d.x0)
+                .attr('y', d => d.y0)
+                .attr('height', d => d.y1 - d.y0)
+                .attr('width', d => d.x1 - d.x0)
+                .attr('fill', d => color(d.name))
+            .append('title')
+                .text(d => `${d.name}\ninput ${format(d.value)} items per second`)
+        ;
 
-			
-		}
-		//console.debug(nodes, recipeId);
+        node.append('text')
+            .attr('fill', d => color(d.name))
+            .attr('x', d => d.x0 < width / 2 ? d.x1 + 6 : d.x0 - 6)
+            .attr('y', d => (d.y1 + d.y0) / 2)
+            .attr('text-anchor', d => d.x0 < width / 2 ? 'start' : 'end')
+            .attr('dy', '0.35em')
+            .text( d => d.name )
+        ;
 
-		//console.groupEnd();
+    }
 
-		return [nodes, links]
-	}
+    // return nodes, links
+    getSourcesFor( recipeId ) {
+        const nodes = new Map<string, any>();
+        const links = [];
+        // console.group(recipeId);
+        nodes.set(recipeId, {id: recipeId, name: this.recipes[recipeId].name});
+
+        for ( const ingr of this.recipes[recipeId].recipe.ingredients ) {
+
+            // nodes.set(ingr.id, {id: ingr.id, name: this.recipes[ingr.id].name});
+            links.push( {
+                source: ingr.id , target: recipeId,
+                value: ingr.amount / this.recipes[recipeId].recipe.time / this.recipes[recipeId].recipe.yield
+            } );
+            const [childNodes, childLinks] = this.getSourcesFor(ingr.id);
+
+            childNodes.forEach(function (v, k, m) {
+                nodes.set(k, v);
+            });
+
+            for ( const chL of childLinks ) { // sum
+                const found = links.find( el => el.source === chL.source && el.target === chL.target );
+                if ( !!found ) {
+                    found.value += chL.value;
+                } else {
+                    links.push( chL );
+                }
+            }
+        }
+        // console.debug(nodes, recipeId);
+
+        // console.groupEnd();
+
+        return [nodes, links];
+    }
 }
